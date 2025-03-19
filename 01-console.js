51090096b83c2d6c79d874d18dcb2d3a26130fd7
sham-sha <sham08092003@gmail.com>
2025-03-19 13:39:42 +0530

    learned about console methods

---
+++ 01-console.js
@@ -0,0 +1,24 @@
+let arr = ["apple", "banana", "orange"]
+let obj = {
+    name:"Sham",
+    age:21,
+    isEmployee:false,
+
+}
+// console.dir(arr)
+
+// console.dir(obj)
+
+console.table(arr)
+
+let name = "sam"
+let name1 = "santhosh"
+let name2 = "jeeva"
+
+console.group("Names")
+console.log(name);
+console.log(name1);
+console.log(name2);
+
+console.groupEnd();
+
